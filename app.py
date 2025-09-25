ab1c4f12e5d020f25099ca04099dc1f6e327260b
Jayden Jiang <jiangjaydenjmx@gmail.com>
2025-09-25 18:11:45 -0400

    feat: slight changes to UI

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ text: list[str] = ['Hello there!', 'Oh, who am I?', 'I am a computer created by 
                    'I am not the best at telling information.',
                    'So you should email him.',
                    "Please don't spam emails or else he will get mad.",
-                   'That is all I have to say. ADIOS, I guess.']
+                   'That is all I have to say. ADIOS, I guess (totally nothing at the bottom-left corner).']
 last_idx: int = len(text) - 1
 
 
