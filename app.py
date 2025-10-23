05db2fcfc4a46262aa775fa70c48b6a2ce0b04f5
Jayden Jiang <jiangjaydenjmx@gmail.com>
2025-10-22 20:41:20 -0400

    feat: add new dialogue and route, fix some styles

--- app.py
+++ app.py
@@ -5,28 +5,11 @@ The script also fetches the portfolio messages to the user. To run the app, run 
 http://127.0.0.1:5000 at the terminal to access the app. Press CTRL+C to stop the app.
 """
 from flask import Flask, render_template, request
+import json
 
 app: Flask = Flask(__name__)
-text: list[str] = ['Hello there!', 'Oh, who am I?', 'I am a computer created by someone.',
-                   'Who is that person, you may ask?',
-                   'That person is jmx-jiang.', 'Yes, that is his name.', 'If you do not know, he is a programmer.',
-                   'He made me to tell you about him.', 'For some reason.',
-                   'Look at these animations!', 'jmx-jiang has the power to create them!', "They're just so awesome!",
-                   'WOW!', 'OK, I am exaggerating too much.', "They're actually very simple.",
-                   "He's actually a lazy person.", "Don't tell him I said that.",
-                   'Also did you know that he came up with this site in his dream?',
-                   'Loading text... SyntaxError: Unexpected symbol: @', "I'm just kidding. That was not an error.",
-                   'Now for some serious information.', 'His favorite language is Python.',
-                   'He builds a variety of things, not just websites.',
-                   'He likes to create interesting websites (like this one).',
-                   'He prefers readability.', 'He likes dark mode.', 'He started learning at 12 years old!',
-                   'This website was built when he was 14!',
-                   "Hey, isn't this a bit boring?",
-                   'I am not the best at telling information.',
-                   'So you should email him.',
-                   "Please don't spam emails or else he will get mad.",
-                   'That is all I have to say.', 'ADIOS, I guess (totally nothing at the bottom-left corner).']
-last_idx: int = len(text) - 1
+with open('dialogue.json') as f:
+    dialogue: dict = json.load(f)
 
 
 @app.route('/')
@@ -39,6 +22,9 @@ def home() -> str:
 def chat() -> str:
     """Generates a page where users see information and decides which message is being used."""
     msg: int = request.args.get('msg', default=0, type=int)
+    text: list = dialogue['intro']
+    last_idx: int = len(text) - 1
+
     if msg < 0 or msg > last_idx:
         return render_template('error.html', images=range(20))
     return render_template(
@@ -55,6 +41,19 @@ def egg() -> str:
     return render_template('egg.html',
                            play=request.args.get('play', default=False, type=bool),
                            clicked=request.args.get('clicked', default=False, type=bool))
+
+
+@app.route('/rampage')
+def rampage() -> str:
+    """Generates the conversation between the computer and the egg. No questions asked."""
+    msg: int = request.args.get('msg', default=0, type=int)
+    text: list = dialogue['rampage']
+    last_idx: int = len(text) - 1
+    return render_template('rampage.html',
+                           msg=text[msg],
+                           id=msg,
+                           is_first=msg == 0,
+                           is_not_last=msg < last_idx)
 
 
 @app.errorhandler(404)
