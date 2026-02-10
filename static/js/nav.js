e4de34eb35e2d89133be1eed75fc8a5a9979a26f
Jayden Jiang <jiangjaydenjmx@gmail.com>
2026-02-09 21:21:38 -0500

    feat: change the key for the next button from 'd' to 'f' to prevent conflicts with moving the computer in the minigame

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,4 +1,4 @@
 $(document).keydown(function(event) {
     if (event.key.toLowerCase() === 'a') $('#previous').click();
-    else if (event.key.toLowerCase() === 'd') $('#next').click();
+    else if (event.key.toLowerCase() === 'f' && $('#next').css('display') !== 'none') $('#next').click();
 });
